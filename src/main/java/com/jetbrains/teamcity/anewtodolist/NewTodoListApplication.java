6e2d8ea7722c6c70abfecf6f88caa32af34989de
Marco Behler <marco@marcobehler.com>
2020-10-15 09:30:27 +0100

    preparing a todolist

--- src/main/java/com/jetbrains/teamcity/anewtodolist/NewTodoListApplication.java
+++ src/main/java/com/jetbrains/teamcity/anewtodolist/NewTodoListApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TheNextGoogleApplication {
+public class NewTodoListApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TheNextGoogleApplication.class, args);
+		SpringApplication.run(NewTodoListApplication.class, args);
 	}
 
 }
