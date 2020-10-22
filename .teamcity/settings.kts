f674ef369d6d1394ce184b69f57dda11a40b23ce
Marco Behler <marco@marcobehler.com>
2020-10-22 09:28:10 +0100

    wip

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -27,13 +27,8 @@ To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
 version = "2020.1"
 
 project {
-    buildType(Build)
-    buildType(Package)
 
-    sequential {
-        buildType(Build)
-        buildType(Package)
-    }
+    buildType(Build)
 }
 
 object Build : BuildType({
@@ -45,29 +40,11 @@ object Build : BuildType({
 
     steps {
         maven {
+            name = "my custom step name"
             goals = "clean test"
             runnerArgs = "-Dmaven.test.failure.ignore=true"
         }
     }
-})
-
-object Package : BuildType({
-    name = "Package"
-
-    vcs {
-        root(DslContext.settingsRoot)
-    }
-
-    steps {
-        maven {
-            goals = "clean package"
-            runnerArgs = "-Dmaven.test.failure.ignore=true"
-        }
-    }
-
-//    dependencies {
-//        snapshot(Build) {}
-//    }
 
     triggers {
         vcs {
