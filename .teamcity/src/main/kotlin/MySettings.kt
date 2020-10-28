f8c9e22912ef2071833764dbed4e7048569a8cac
Marco Behler <marco.behler@jetbrains.com>
2020-10-28 17:51:12 +0100

    wip

--- .teamcity/src/main/kotlin/MySettings.kt
+++ .teamcity/src/main/kotlin/MySettings.kt
@@ -10,7 +10,7 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
 object MyProject: Project({
     val bts = sequential {
 
-        buildType(Maven("Test DSL", "clean test", ".teamcity/pom.xml"))
+        buildType(Maven("Test DSL", "clean test", null, ".teamcity/pom.xml"))
         buildType(Maven("Build", "clean compile"))
         parallel {
             buildType(Maven("Slow Test", "clean test", "-Dtest=*.integration.*Test"))
