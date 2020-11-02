8dedad7083dd639b668090572f6a777b3ef915fd
Marco Behler <marco@marcobehler.com>
2020-11-02 14:32:49 +0000

    wip

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -40,8 +40,7 @@ object Build : BuildType({
 
     steps {
         maven {
-            val myMavenGoals = "clean test"
-            goals = myMavenGoals
+            goals = "clean package"
             runnerArgs = "-Dmaven.test.failure.ignore=true"
         }
     }
